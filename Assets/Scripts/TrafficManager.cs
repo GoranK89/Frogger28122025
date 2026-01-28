c306eca860c320de5c0d70203645d67bf0ec26e9
gkolar <goran.kolar7@gmail.com>
2026-01-28 11:26:35 +0100

    Cars are now spawning and moving

--- Assets/Scripts/TrafficManager.cs
+++ Assets/Scripts/TrafficManager.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public class TrafficManager : MonoBehaviour {
-    [SerializeField] private GameObject[] carPrefabs;
+    [SerializeField] private GameObject[] vehiclePrefabs;
 
-    [SerializeField] private float minSpawnDelay = 2f;
-    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 2f;
 
     private int spawnXleft = -1;
     private int spawnXright = 16;
@@ -15,8 +15,19 @@ public class TrafficManager : MonoBehaviour {
     private int carsSpawned = 0;
     private int maxCars = 20;
 
+    // Car Rotation
+    private bool driveLeft;
+
     private void Start() {
         StartCoroutine(SpawnVehiclesRandomly());
+    }
+
+    private void Update() {
+        // TODO: this is inefficient, ok for testing
+        GameObject[] vehicles = GameObject.FindGameObjectsWithTag("Vehicle");
+        foreach (GameObject vehicle in vehicles) {
+            MoveVehicle(vehicle);
+        }
     }
 
     private IEnumerator SpawnVehiclesRandomly() {
@@ -24,17 +35,37 @@ public class TrafficManager : MonoBehaviour {
             float randomDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
             yield return new WaitForSeconds(randomDelay);
 
-            int randomLeftY = leftSpawnRows[Random.Range(0, leftSpawnRows.Length)];
-            int randomRightY = rightSpawnRows[Random.Range(0, rightSpawnRows.Length)];
+            bool spawnOnLeft = Random.value > 0.5f;
 
-            VehicleSpawner(spawnXleft, randomLeftY);
-            VehicleSpawner(spawnXright, randomRightY);
+            if (spawnOnLeft) {
+                int randomLeftY = leftSpawnRows[Random.Range(0, leftSpawnRows.Length)];
+                VehicleSpawner(spawnXleft, randomLeftY);
+            }
+            else {
+                int randomRightY = rightSpawnRows[Random.Range(0, rightSpawnRows.Length)];
+                VehicleSpawner(spawnXright, randomRightY);
+            }
+
             carsSpawned++;
         }
     }
 
     private void VehicleSpawner(int x, int y) {
         Vector3 spawnPosition = GameManager.Instance.grid.GetCellCenterWorldPosition(x, y);
-        Instantiate(carPrefabs[Random.Range(0, carPrefabs.Length)], spawnPosition, Quaternion.identity);
+        if (x == spawnXright) {
+            driveLeft = true;
+            GameObject vehicle = Instantiate(vehiclePrefabs[Random.Range(0, vehiclePrefabs.Length)], spawnPosition,
+                Quaternion.Euler(0, 180, 0));
+        }
+        else {
+            driveLeft = false;
+            GameObject vehicle = Instantiate(vehiclePrefabs[Random.Range(0, vehiclePrefabs.Length)], spawnPosition,
+                Quaternion.identity);
+        }
+    }
+
+    private void MoveVehicle(GameObject vehicle) {
+        float speed = 4f * Time.deltaTime;
+        vehicle.transform.Translate(Vector3.right * speed);
     }
 }
